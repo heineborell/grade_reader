863fd0940fefbf909df40c31d24f8d168193f088
Deniz Deveci <dodeve@gmail.com>
2025-08-06 22:55:25 +0900

    support for wayland

--- main.py
+++ main.py
@@ -1,32 +1,17 @@
 import cv2
-from manip import image_manip
+from manip import image_manip, get_center
 
 # read image make it grayscaled
 img_path = "data/test_2.jpg"
 
 img = cv2.imread(img_path)
 morph = image_manip(img_path)
+result, centers = get_center(img, morph)
 
 
-# # get contours
-result = img.copy()
-centers = []
-
-# so this guy return contours which are the boundaries between black and white regions. But it basically returns bunch of stuff depending on the opencv you use but using return-2 we guarantee it returns the contours
-contour_results = cv2.findContours(morph, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-contours = contour_results[-2]
-
-# here using first and zeroth moments we find the center for each contour, then draw a circle at each point
-for i, cntr in enumerate(contours):
-    M = cv2.moments(cntr)
-    cx = int(M["m10"] / M["m00"])
-    cy = int(M["m01"] / M["m00"])
-    centers.append((cx, cy))
-    cv2.circle(result, (cx, cy), 20, (136, 231, 136), -1)
-    pt = (cx, cy)
-    print("circle #:", i, "center:", pt)
-
-cv2.imshow("result", result)
+cv2.namedWindow("Window", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("Window", 1200, 800)
+cv2.imshow("Window", result)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- src/manip.py
+++ src/manip.py
@@ -14,3 +14,27 @@ def image_manip(image_path):
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
     morph = cv2.morphologyEx(morph, cv2.MORPH_CLOSE, kernel)
     return morph
+
+
+def get_center(og_img, morphed_img):
+    # # get contours
+    result = og_img.copy()
+    centers = []
+
+    # This guy return contours which are the boundaries between black and white regions. But it basically returns bunch of stuff depending on the opencv you use but using return-2 we guarantee it returns the contours
+    contour_results = cv2.findContours(
+        morphed_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
+    )
+    contours = contour_results[-2]
+
+    # here using first and zeroth moments we find the center for each contour, then draw a circle at each point
+    for i, cntr in enumerate(contours):
+        M = cv2.moments(cntr)
+        cx = int(M["m10"] / M["m00"])
+        cy = int(M["m01"] / M["m00"])
+        centers.append((cx, cy))
+        cv2.circle(result, (cx, cy), 20, (136, 231, 136), -1)
+        pt = (cx, cy)
+        print("circle #:", i, "center:", pt)
+
+    return result, centers
