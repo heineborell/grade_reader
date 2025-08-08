8cca2613185b217bfce3adc26137910db1f6bbd5
DENIZ DEVECIOGLU <dodeve@gmail.com>
2025-08-08 16:27:17 +0900

    working with transformation

--- aruco.py
+++ aruco.py
@@ -1,9 +1,6 @@
 import cv2
 import numpy as np
 
-# Load image
-# img = cv2.imread("data/my_test.jpg")
-# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
 parameters = cv2.aruco.DetectorParameters()
@@ -15,7 +12,7 @@ marker_size_pixels = 20  # Size of the marker image in pixels
 # Generate the marker image
 marker_image = cv2.aruco.generateImageMarker(dictionary, marker_id, marker_size_pixels)
 
-img = cv2.imread("data/IMG_2722.jpg")
+img = cv2.imread("data/IMG_2757.jpg")
 
 # ArUco detection
 corners, ids, _ = detector.detectMarkers(img)
@@ -23,10 +20,13 @@ corners = np.intp(corners)
 cv2.polylines(img, corners, True, (136, 231, 136), 1)
 if ids is not None:
     marker_corners = corners[0][0]  # shape (4, 2)
+    # Aruco perimeter which will used to fix the bounding box size
+    aruco_perimeter = cv2.arcLength(corners[0], True)
+
+    # Pixel to cm ratio
+    pixel_cm_ratio = aruco_perimeter / 4
 
     # Define the ROI relative to the marker
-    # Assume OMR region starts 100 pixels right and 150 pixels down from marker
-    # and is 500x300 in size in the marker's coordinate space
     marker_pts = np.array(
         [
             marker_corners[0],  # top-left of marker
@@ -38,14 +38,28 @@ if ids is not None:
     )
 
     # Destination coordinates in marker-aligned space
-    dst_pts = np.array([[0, 0], [200, 0], [200, 200], [0, 200]], dtype=np.float32)
+    dst_pts = np.array(
+        [
+            [0, 0],
+            [pixel_cm_ratio, 0],
+            [1 * pixel_cm_ratio, pixel_cm_ratio],
+            [0, pixel_cm_ratio],
+        ],
+        dtype=np.float32,
+    )
 
     # Get transformation from marker to ideal square
     M = cv2.getPerspectiveTransform(marker_pts, dst_pts)
 
     # Define the bounding box (relative to marker-aligned space)
     form_box = np.array(
-        [[0, 200], [900, 200], [900, 1000], [0, 1000]], dtype=np.float32
+        [
+            [0, pixel_cm_ratio],
+            [4.3 * pixel_cm_ratio, pixel_cm_ratio],
+            [4.3 * pixel_cm_ratio, pixel_cm_ratio + 4.1 * pixel_cm_ratio],
+            [0, pixel_cm_ratio + 4.1 * pixel_cm_ratio],
+        ],
+        dtype=np.float32,
     )
 
     # Transform back to image space
@@ -54,13 +68,12 @@ if ids is not None:
         np.int32(form_box_img[0][1]) : np.int32(form_box_img[2][1]),
         np.int32(form_box_img[0][0]) : np.int32(form_box_img[1][0]),
     ]
-    print(form_box_img)
 
     # Draw the bounding box
     for i in range(4):
         pt1 = tuple(np.int32(form_box_img[i]))
         pt2 = tuple(np.int32(form_box_img[(i + 1) % 4]))
-        cv2.line(img, pt1, pt2, (0, 255, 0), 5)
+        cv2.line(img, pt1, pt2, (0, 255, 0), 3)
 #
 cv2.namedWindow("Window", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("Window", 1200, 800)
