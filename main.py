9ed51c30eb33d30c3c1d7a58f1220ad8966c9ccb
DENIZ DEVECIOGLU <dodeve@gmail.com>
2025-10-19 18:50:25 +0900

    working everything

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from sheetmerger import merge_grade
 def main():
     for student_no, grade in getGradeStudentId(static=False):
         print("Detected:", student_no, grade)
-        merge_grade(
-            "gc_2025CMN17.20BS105a01_fullgc_2025-10-19-16-43-45.csv",
-            (student_no, grade),
-        )
+        if student_no and grade is not None:
+            merge_grade(
+                "gc_2025CMN17.20BS105a01_fullgc_2025-10-19-16-43-45.csv",
+                (student_no, grade),
+            )
 
 
 if __name__ == "__main__":
--- src/sheetmerger.py
+++ src/sheetmerger.py
@@ -2,6 +2,7 @@ import subprocess
 import tools
 import pandas as pd
 import xlwt
+from pathlib import Path
 
 
 # get the column ids from original dataframe to replace them with simpler readeable ones
@@ -15,12 +16,34 @@ def push_values(df, values):
     orig_columns = list(df.columns)
     column_name = orig_columns[get_column_ids(orig_columns, "midterm")]
     df.loc[df["Username"] == int(values[0]), column_name] = values[1]
-    return df
+    return (df, column_name)
 
 
 def merge_grade(df_path, values):
     # Load your LMS datasets
+    save_path = Path("final.csv")
     df = pd.read_csv(df_path)
-    df = push_values(df, values)
+    pushed = push_values(df, values)
+    df = pushed[0]
+    column_name = pushed[1]
+    # subprocess.run(["vd", "-f", "csv", "-"], input=df.to_csv(index=True), text=True)
 
-    subprocess.run(["vd", "-f", "csv", "-"], input=df.to_csv(index=True), text=True)
+    if save_path.exists():
+        print("path exist")
+        final_df = pd.read_csv(save_path)
+
+        # Make both DataFrames indexed by Username for alignment
+        final_df.set_index("Username", inplace=True)
+        df.set_index("Username", inplace=True)
+
+        # Update only matching rows/columns
+        final_df.update(df[[column_name]])
+
+        # Write back to file
+        final_df.reset_index(inplace=True)
+        final_df.to_csv(save_path, index=False)
+
+    else:
+        with open("final.csv", "w", encoding="utf-8", newline="") as f:
+            print("path doesnt exist")
+            df.to_csv(f, index=False)
