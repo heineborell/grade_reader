fb8af43021a547b11362f8bd7486b854731b38e5
DENIZ DEVECIOGLU <dodeve@gmail.com>
2025-10-21 14:16:58 +0900

    added other files too

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ from sheetmerger import merge_grade
 
 
 def main():
-    for student_no, grade in getGradeStudentId(static=False):
+    csv_paths = [
+        "gc_2025CMN17.20BS105a01_fullgc_2025-10-19-16-43-45.csv",
+        "gc_2025CMN17.20BS105a02_fullgc_2025-10-21-13-45-52.csv",
+        "gc_2025CMN17.20BS105a03_fullgc_2025-10-21-14-15-07.csv",
+    ]
+    for student_no, grade in getGradeStudentId(static=True):
         print("Detected:", student_no, grade)
         if student_no and grade is not None:
-            merge_grade(
-                "gc_2025CMN17.20BS105a01_fullgc_2025-10-19-16-43-45.csv",
-                (student_no, grade),
-            )
+            for i, path in enumerate(csv_paths):
+                merge_grade(
+                    path,
+                    i,
+                    (student_no, grade),
+                )
 
 
 if __name__ == "__main__":
--- src/sheetmerger.py
+++ src/sheetmerger.py
@@ -13,44 +13,50 @@ def get_column_ids(orig_columns, column_name):
     return id[0]
 
 
-def push_values(df, values):
+def push_values(df, values, section_no):
     orig_columns = list(df.columns)
     column_name = orig_columns[get_column_ids(orig_columns, "midterm")]
+    print(df["Username"])
+    print(type(int(values[0])))
     if (df["Username"] == int(values[0])).any():
         print("[green]The student number exists![/green]")
         df.loc[df["Username"] == int(values[0]), column_name] = values[1]
         print(df["First Name"][df.loc[df["Username"] == int(values[0])].index[0]])
     else:
         print("[red]The student doesnt number exist![/red]")
+        section_no = None
 
-    return (df, column_name)
+    return (df, column_name, section_no)
 
 
-def merge_grade(df_path, values):
+def merge_grade(df_path, section_no, values):
     # Load your LMS datasets
-    save_path = Path("final.csv")
+    save_path = Path(f"final_{section_no}.csv")
     df = pd.read_csv(df_path)
-    pushed = push_values(df, values)
+    pushed = push_values(df, values, section_no)
     df = pushed[0]
     column_name = pushed[1]
     # subprocess.run(["vd", "-f", "csv", "-"], input=df.to_csv(index=True), text=True)
 
-    if save_path.exists():
-        print("path exist")
-        final_df = pd.read_csv(save_path)
+    if pushed[2] is not None:
+        if save_path.exists():
+            print("path exist")
+            final_df = pd.read_csv(save_path)
 
-        # Make both DataFrames indexed by Username for alignment
-        final_df.set_index("Username", inplace=True)
-        df.set_index("Username", inplace=True)
+            # Make both DataFrames indexed by Username for alignment
+            final_df.set_index("Username", inplace=True)
+            df.set_index("Username", inplace=True)
 
-        # Update only matching rows/columns
-        final_df.update(df[[column_name]])
+            # Update only matching rows/columns
+            final_df.update(df[[column_name]])
 
-        # Write back to file
-        final_df.reset_index(inplace=True)
-        final_df.to_csv(save_path, index=False)
+            # Write back to file
+            final_df.reset_index(inplace=True)
+            final_df.to_csv(save_path, index=False)
 
-    else:
-        with open("final.csv", "w", encoding="utf-8", newline="") as f:
-            print("path doesnt exist")
-            df.to_csv(f, index=False)
+        else:
+            with open(
+                f"final_{section_no}.csv", "w", encoding="utf-8", newline=""
+            ) as f:
+                print("path doesnt exist")
+                df.to_csv(f, index=False)
